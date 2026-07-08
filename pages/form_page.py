e2220d5cb3ed5fb36da9b9e7b7160d7f5c89fa3b
Aviral-QA <aviralgupta0744@outlook.com>
2026-07-08 12:42:04 +0530

    Update form pages 4

--- pages/form_page.py
+++ pages/form_page.py
@@ -5,6 +5,8 @@ class FormPage:
         self.page = page
         self.name_field = page.locator("#name")
         self.email_field = page.locator("#email")
+        self.phone_field = page.locator("#phone")
+        self.address_field = page.locator("#textarea")
         self.tuesday_checkbox = page.locator("#tuesday")
         self.country_dropdown = page.locator("#country")
 
@@ -16,6 +18,12 @@ class FormPage:
 
     def fill_email(self, email):
         self.email_field.fill(email)
+
+    def fill_phone(self, phone):
+        self.phone_field.fill(phone)
+
+    def fill_address(self, textarea):
+        self.address_field.fill(textarea)
 
     def check_tuesday(self):
         self.tuesday_checkbox.check()
--- tests/test_form_fields.py
+++ tests/test_form_fields.py
@@ -9,8 +9,14 @@ def test_fill_name_field(page):
 def test_fill_email_field(page):
     form = FormPage(page)
     form.goto()
-    form.fill_name("aviral@gmail.com")
+    form.fill_email("aviral@gmail.com")
     assert form.name_field.input_value() == "aviral@gmail.com"
+
+def test_fill_phone_field(page):
+    form = FormPage(page)
+    form.goto()
+    form.fill_phone("9639348524")
+    assert form.name_field.input_value() == "9639348524"
 
 def test_check_tuesday_checkbox(page):
     form = FormPage(page)
